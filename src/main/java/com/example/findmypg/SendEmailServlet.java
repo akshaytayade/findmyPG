fc45708482ba279c83cdacf2a7ea622f7455f3a7
akshaytayade <tayade_akshay@outlook.com>
2023-11-22 02:57:28 +0530

    masking

--- src/main/java/com/example/findmypg/SendEmailServlet.java
+++ src/main/java/com/example/findmypg/SendEmailServlet.java
@@ -18,26 +18,23 @@ public class SendEmailServlet extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Loaded into Email Servlet");
-        //provide recipient's email ID
-        String from = "findmypgdetails@gmail.com";
-        //provide sender's email ID
+
+        ServletContext sc = request.getServletContext();
+
+        //Setting Properties
+        String from = sc.getInitParameter("FROM_EMAIL");
+        final String username = sc.getInitParameter("FROM_EMAIL");
+        final String password = sc.getInitParameter("EMAIL_PASSWORD");
+        String host = sc.getInitParameter("EMAIL_SMTP");
+
         String to = "iamakshay938@gmail.com";
-        //provide Mailtrap's username
-        final String username = "findmypgdetails@gmail.com";
-        //provide Mailtrap's password
-        final String password = "fton tjuf zwos xgmy";
-        //provide Mailtrap's host address
-        String host = "smtp.gmail.com";
+
         //configure Mailtrap's SMTP server details
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.port", "587");
-
-        //convert to char array
-        char[] passwordChars = password.toCharArray();
-
 
         //create the Session object
         Authenticator authenticator = new Authenticator() {
@@ -47,18 +44,12 @@ public class SendEmailServlet extends HttpServlet {
         };
         Session session = Session.getInstance(props, authenticator);
         try {
-            //create a MimeMessage object
             Message message = new MimeMessage(session);
-            //set From email field
             message.setFrom(new InternetAddress(from));
-            //set To email field
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(to));
-            //set email subject field
-            message.setSubject("Here comes Jakarta Mail!");
-            //set the content of the email message
+            message.setSubject("findmyPG | Property Details for: ");
             message.setContent("Just discovered that Jakarta Mail is fun and easy to use", "text/html");
-            //send the email message
             Transport.send(message);
             System.out.println("Email Message Sent Successfully");
         } catch (MessagingException e) {
